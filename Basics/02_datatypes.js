c096b830f87e7513a350f6def6754980d402501c
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-09-06 22:25:56 +0000

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// alert(4+55) functional in browser, not in node env.
+// ECMA doc -> tc39.com -> original js language standered. (FOR BROWSER DEV)
+
+// MDN docs -> created by mozella. (RECOMMENDED FOR SOFTWARE DEV)
+
+
+
+// number -> 2^53
+// BigInt
+// String -> ""
+// boolean
+// null -> standalone value
+// undefined -> value is not yet assigned.
+// symbol -> used to idicate unique parts.
+
+
+// objects -> group of other datatypes.
+
+
+
+console.log(typeof null); //-> object.
+console.log(typeof undefined); //-> undefined.
