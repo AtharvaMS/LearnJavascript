f8253de8a088e07052673b3d76d376b95c8c8e13
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-09-07 13:26:35 +0000

    datatype conversion

---
+++ Basics/03_datatypesConversion.js
@@ -0,0 +1,21 @@
+let value = "true"
+
+console.log(value);
+console.log(typeof value);
+
+let valueOfValue = Number(value)
+
+console.log(valueOfValue);
+console.log(typeof valueOfValue);
+
+// "33" => 33
+//  "45ab" => NaN but type is number
+// true => 1 and false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn) //=> successful conversion from number to boolean
+
+
+
