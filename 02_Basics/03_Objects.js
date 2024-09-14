c7382288b7b27cc496a27317dba4721107ff255f
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-09-14 17:49:04 +0000

    ..

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -20,7 +20,14 @@ const obj2 = {3:'c',4:'d'}
 // const obj3 = Object.assign({}, obj1, obj2) //-> (target, source1, source2, ...) (rarely used)
 // console.log(obj3);
 
-const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+//keys and values returns array
+console.log(Object.keys(ytuser));
+console.log(Object.values(ytuser));
+console.log(Object.entries(ytuser)); //-> key:value -> array pairs are created
+
 
 
