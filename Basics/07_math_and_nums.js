7dc1901de3aaae93b67b0af3cf43c8d2cca3d010
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-09-14 12:35:22 +0000

    math

---
+++ Basics/07_math_and_nums.js
@@ -0,0 +1,15 @@
+const score = 155.855
+
+console.log(score.toString());
+console.log(score.toString().length);
+console.log(score.toFixed(23));
+
+console.log(score.toPrecision(3));
+
+
+const scoreLatest = 1000000
+console.log(scoreLatest.toLocaleString('en-IN'));
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+
