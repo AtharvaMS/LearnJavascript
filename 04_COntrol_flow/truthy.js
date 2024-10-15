c4c025febf31cc75df739779b8abdbe77a4762b5
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-10-15 18:04:08 +0000

    truthy : null coalescing operator

---
+++ 04_COntrol_flow/truthy.js
@@ -0,0 +1,6 @@
+// Nullish coalescing Operator (??) : null undefined
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10 // -> Generally in databses, if response is not there then it will assign second funciton, value, etc in val1 variable.
+console.log(val1);
