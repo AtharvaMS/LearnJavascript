8a7c153aa2df734a09d34557f7646fd81406408f
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-10-15 18:06:48 +0000

    truthy

--- 04_COntrol_flow/truthy.js
+++ 04_COntrol_flow/truthy.js
@@ -4,3 +4,10 @@ let val1;
 val1 = 5 ?? 10
 val1 = null ?? 10 // -> Generally in databses, if response is not there then it will assign second funciton, value, etc in val1 variable.
 console.log(val1);
+
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//Truthy values
+// "0", "false", " ", [], {}, function(){}
