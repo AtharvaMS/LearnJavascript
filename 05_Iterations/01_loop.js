dedcda529deff54b3b0e4d59a446168c204aa834
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-10-23 12:29:59 +0000

    Loops

---
+++ 05_Iterations/01_loop.js
@@ -0,0 +1,7 @@
+
+
+for (let index = 0; index < 100; index+=10) {
+   for (let j = 0; j < 10; j++) {
+    console.log(`values : ${index+j}`);  
+   }
+}
---
+++ 05_Iterations/02_while.js
@@ -0,0 +1,22 @@
+
+let names = "Atharva"
+// // let i = 10
+// while(i>0){
+//     console.log(`hello this is ${names + i} `);
+//     i-=2;
+// }
+
+
+
+let i = 0
+do{
+    if(i == 0){
+        console.log(`Batman Help!`);
+        
+    }else{
+        console.log(`HELP! ${i}`);
+    }
+   
+    i++;
+    
+}while(i<20)
